119b739a9ef4703ed32f3da555771b4628b023ed
Martin Gran <martgra@gmail.com>
2020-09-15 10:05:35 +0000

    added new methods for fpl package for io and visualization added setup.py to install package

---
+++ fpl/data/io.py
@@ -0,0 +1,57 @@
+import json
+from pathlib import Path
+import os
+
+import pandas as pd
+
+
+def load_json(file_path):
+    """Load json file
+
+    Args:
+        file_path (str): path to file in format .json
+
+    Returns:
+        dict: decoded json
+    """    
+    with open(Path(file_path)) as json_file:
+            try: 
+                return json.load(json_file)
+            except json.decoder.JSONDecodeError:
+                print("Unable to decode json")
+
+
+def dump_json(file_path):
+    pass
+
+
+def load_dataframe_from_json(file_path, object_to_grab):
+    """load JSON into dataframe
+
+    Args:
+        file_path (str): Path to .json file
+        object_to_grab (stri): Key to object to grab
+
+    Returns:
+        pandas.DataFrame: dataframe holding json-object
+    """    
+    json_object = load_json(file_path)
+    try:
+        return pd.DataFrame(json_object[str(object_to_grab)])
+    except Exception:
+        print("Could not load dataframe")
+    except KeyError:
+        print("Key not found")
+
+
+def list_data_dir(dir_path, suffix=".json"):
+    """List content of a data directory
+
+    Args:
+        dir_path (str): Path to directory to list
+        suffix (str, optional): Filter content based on file suffix. Defaults to ".json".
+
+    Returns:
+        list: List holding names of files in directory 
+    """      
+    return sorted([Path(dir_path,i) for i in os.listdir(dir_path) if Path(i).suffix == suffix])
---
+++ fpl/visualization/exploration.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from IPython.display import display
+
+
+def reorder_elements(data_frame):
+    try:
+        _elements_columns = sorted(data_frame.columns.tolist())
+        _elements_columns.insert(0, _elements_columns.pop(_elements_columns.index("team")))
+        _elements_columns.insert(0, _elements_columns.pop(_elements_columns.index("web_name")))
+        return data_frame[_elements_columns]
+    except:
+        print("Not able to shuffle elements table, check if data_frame holds elements")
+
+
+def compare_elements(snapshot_list, element_id, element_value):
+    """display row(s) from successive snapshots  
+
+    Args:
+        snapshot_list (list): List holding pandas.DataFrame
+        element_id (str): Element(s) to select
+    """    
+    _df_temp = snapshot_list[0].loc[snapshot_list[0][element_id] == element_value]
+    for i in snapshot_list:
+        _df_temp = _df_temp.append(i.loc[i[element_id] == element_value], ignore_index=True)
+    display(_df_temp)
