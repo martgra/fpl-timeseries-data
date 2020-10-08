620e5be2ebe6bf1cbd6b7df6334ebf5ba0781296
Martin Gran <martgra@gmail.com>
2020-10-08 08:20:09 +0200

    Fix entry point (#10)
    * adding top penalty takers
    and exported folder to gitignore
    
    * removing redundant file
    
    * added github actions
    
    * adding change to actions
    
    * fixing actions file
    
    * added covconv
    
    * updating actions
    
    * updating actions
    
    * fixing workflow
    
    * fixing in workflow
    
    * making contributing.md
    
    * added contributing.md and readme.md
    added a WA to make azure_storage to work with anonymous-read only string
    
    * fixed typo in readme
    
    * fixed setup.py
    
    * added click to requirments
    
    * fixed entrypoint in setup.py

--- fpl/__main__.py
+++ fpl/__main__.py
@@ -1,4 +1,11 @@
 """Main module."""
 from fpl.command_line.cli import register_cli
 
-register_cli()
+
+def main():
+    """Pull it all together."""
+    register_cli()
+
+
+if __name__ == "__main__":
+    main()
